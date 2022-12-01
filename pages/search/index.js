5d6f02f35dea606b854fa7ba77cbb9ee801e2922
david santos <56547716+daviddevolin@users.noreply.github.com>
2022-12-01 11:24:47 -0300

    inserindo forms

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,25 +1,61 @@
-import { Space, Input, Button } from "antd";
+import { Space, Input, Button , Form} from "antd";
 import { SearchOutlined } from '@ant-design/icons';
 import "antd/dist/antd.css";
-export default function Search(){
-    const onClickHandler = (e) => {
-        e.preventDefault() 
+export default function Search(){   
+    const onFinish =()=>{
         const value = document.getElementById('inputFilme').value;
         window.location.href= `/searchmovies/${value}`
     }
-    const onKeyEnter = (e) => {
-        if(e.charCode === 13){
-            const value = document.getElementById('inputFilme').value;
-            window.location.href= `/searchmovies/${value}`
-        } 
-    }
+
+    const [form] = Form.useForm();
+
+    const formItemLayout = {
+        labelCol: {
+          span: 4,
+        },
+        wrapperCol: {
+          span: 8,
+        },
+    };
+
+    const formTailLayout = {
+        labelCol: {
+          span: 4,
+        },
+        wrapperCol: {
+          span: 8,
+          offset: 4,
+        },
+    };
     return(
-        <div> 
-            <Space direction="horizontal" style={{width: '100%', justifyContent: 'center'}}>
-                <Input placeholder="digite o nome do filme"  id="inputFilme" onKeyPress={onKeyEnter}/>    
-                <Button type='primary'  icon={<SearchOutlined />} onClick={onClickHandler} id='buttonInput'> pesquisar</Button>
-            </Space>
-            
+        <div>
+            <Form form={form} name="dynamic_rule" onFinish={onFinish}>
+                <Form.Item
+                    {...formItemLayout}
+                    name="username"
+                    label="Name"
+                    rules={[
+                    {
+                        required: true,
+                        message: 'insira o nome do filme',
+                    },
+                    ]}
+                >
+                    <Input 
+                        placeholder="digite o nome do filme"  
+                        id="inputFilme" 
+                    />
+                </Form.Item>
+
+                <Form.Item {...formTailLayout}>
+                <Button 
+                    type='primary'  
+                    icon={<SearchOutlined />}
+                    htmlType="submit" 
+                    id='buttonInput'> pesquisar
+                </Button>
+                </Form.Item>
+            </Form>
         </div>
     )
     
